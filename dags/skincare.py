35b094dbb7481af720e110b66167093e32ee2468
hasnaa.bd <75904146+hasnaabouagad@users.noreply.github.com>
2025-05-08 20:44:43 +0100

    Update skincare.py

--- dags/skincare.py
+++ dags/skincare.py
@@ -13,16 +13,16 @@ from airflow.models.baseoperator import chain
 
 
 pyspark_job = {
-    "reference": {"project_id": "spheric-entity-457510-t4"},
+    "reference": {"project_id": "PROJECT_ID"},
     "placement": {"cluster_name": "skincare-dataproc-cluster"},
     "pyspark_job": {
-        "main_python_file_uri": "gs://skin-care-pipeline-spheric-entity-457510-t4/spark/Spark_Processing.py",
+        "main_python_file_uri": "gs://BUCKET_NAME/spark/Spark_Processing.py",
     },
 }
 
 @dag(
 
-    start_date=datetime(2025, 4, 22),
+    start_date=datetime(2025, 5, 8),
     schedule=None,
     catchup=False,
     tags=['skincare'],
@@ -34,7 +34,7 @@ def skincare():
         task_id='upload_products',
         src='/usr/local/airflow/include/dataset/product_info.csv',
         dst='raw/product_info.csv',
-        bucket='skin-care-pipeline-spheric-entity-457510-t4',
+        bucket='BUCKET_NAME',
         gcp_conn_id='gcp',
         mime_type='text/csv',
         execution_timeout=timedelta(minutes=30),
@@ -44,7 +44,7 @@ def skincare():
         task_id="upload_reviews_1",
         src="/usr/local/airflow/include/dataset/reviews_0-250.csv",
         dst="raw/reviews_0-250.csv",
-        bucket="skin-care-pipeline-spheric-entity-457510-t4",
+        bucket="BUCKET_NAME",
         gcp_conn_id='gcp',
         mime_type='text/csv',
         execution_timeout=timedelta(minutes=30),
@@ -54,7 +54,7 @@ def skincare():
         task_id="upload_reviews_2",
         src="/usr/local/airflow/include/dataset/reviews_250-500.csv",
         dst="raw/reviews_250-500.csv",
-        bucket="skin-care-pipeline-spheric-entity-457510-t4",
+        bucket="BUCKET_NAME",
         gcp_conn_id='gcp',
         mime_type='text/csv',
         execution_timeout=timedelta(minutes=30),
@@ -64,7 +64,7 @@ def skincare():
         task_id="upload_reviews_3",
         src="/usr/local/airflow/include/dataset/reviews_500-750.csv",
         dst="raw/reviews_500-750.csv",
-        bucket="skin-care-pipeline-spheric-entity-457510-t4",
+        bucket="BUCKET_NAME",
         gcp_conn_id='gcp',
         mime_type='text/csv',
         execution_timeout=timedelta(minutes=30),
@@ -75,7 +75,7 @@ def skincare():
         task_id="upload_reviews_4",
         src="/usr/local/airflow/include/dataset/reviews_750-1250.csv",
         dst="raw/reviews_750-1250.csv",
-        bucket="skin-care-pipeline-spheric-entity-457510-t4",
+        bucket="BUCKET_NAME",
         gcp_conn_id='gcp',
         mime_type='text/csv',
         execution_timeout=timedelta(minutes=30),
@@ -85,7 +85,7 @@ def skincare():
         task_id="upload_reviews_5",
         src="/usr/local/airflow/include/dataset/reviews_1250-end.csv",
         dst="raw/reviews_1250-end.csv",
-        bucket="skin-care-pipeline-spheric-entity-457510-t4",
+        bucket="BUCKET_NAME",
         gcp_conn_id='gcp',
         mime_type='text/csv',
         execution_timeout=timedelta(minutes=30),
@@ -93,9 +93,9 @@ def skincare():
 
     upload_spark_job = LocalFilesystemToGCSOperator(
         task_id="upload_spark_job",
-        src="/usr/local/airflow/include/dataset/Spark_Processing.py",
-        dst="spark/Spark_Processing.py",
-        bucket="skin-care-pipeline-spheric-entity-457510-t4",
+        src="/usr/local/airflow/include/dataset/PySpark_Processing.py",
+        dst="spark/PySpark_Processing.py",
+        bucket="BUCKET_NAME",
         gcp_conn_id='gcp',
         mime_type='text/x-python',
         execution_timeout=timedelta(minutes=30),
@@ -105,13 +105,13 @@ def skincare():
     task_id="submit_spark_job",
     job=pyspark_job,
     region="europe-west1",
-    project_id="spheric-entity-457510-t4",
+    project_id="PROJECT_ID",
     gcp_conn_id="gcp",
 )
 
     delete_dataproc_cluster = DataprocDeleteClusterOperator(
     task_id="delete_dataproc_cluster",
-    project_id="spheric-entity-457510-t4",
+    project_id="PROJECT_ID",
     region="europe-west1",
     cluster_name="skincare-dataproc-cluster",
     gcp_conn_id="gcp",
